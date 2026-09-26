e5284f0bd45ce50008581463463d840e7a00fd1b
nisha24jan-oss <nisha24jan@gmail.com>
2026-09-26 09:05:28 +0530

    Add files via upload

---
+++ Password_strength.py
@@ -0,0 +1,33 @@
+import streamlit as st
+
+st.title("🔐 Password Strength Checker")
+
+password = st.text_input(
+    "Enter your password:",
+    type="password"
+)
+
+if password:
+    score = 0
+
+    if len(password) >= 8:
+        score += 1
+
+    if any(char.isupper() for char in password):
+        score += 1
+
+    if any(char.islower() for char in password):
+        score += 1
+
+    if any(char.isdigit() for char in password):
+        score += 1
+
+    if any(char in "!@#$%^&*" for char in password):
+        score += 1
+
+    if score <= 2:
+        st.error("🔴 Weak Password")
+    elif score <= 4:
+        st.warning("🟡 Medium Password")
+    else:
+        st.success("🟢 Strong Password")
